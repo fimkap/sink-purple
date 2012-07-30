be817de991682a67e269b930fa854ad55e37c17f
fimkap <fimkap@gmail.com>
2012-07-30 23:17:40 +0300

    JSONP script for download_contacts.

---
+++ download_contacts.php
@@ -0,0 +1,19 @@
+<?php
+
+$url = "http://purple-dev.appspot.com/api/120112/DownloadContacts";
+$ch = curl_init();
+ 
+$pfields = 'session_token=' . $_GET['session_token'] . '&since=' . $_GET['since'] . '&from=' . $_GET['from'] . '&max_results=' . $_GET['max_results'];
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $pfields);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADERS, array('Content-Type: application/x-www-form-urlencoded'));
+ 
+$response = curl_exec($ch);
+//echo $response;
+
+curl_close($ch);
+echo $_GET['callback'] . '(' . $response . ');';
+
+?>
